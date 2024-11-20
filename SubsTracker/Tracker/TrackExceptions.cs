4f14da9847db9014c66f0f0e71ddc360c2d2f847
Антон Ермаков <Cheshir.o.T@gmail.com>
2024-11-20 03:50:42 +0300

    tracker

---
+++ SubsTracker/Tracker/TrackExceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubsTracker.Users;
+
+namespace SubsTracker.Logs
+{
+    class TrackExceptions
+    {
+        public static void TrackException(string ex)
+        {
+            Console.WriteLine($"ex: {ex}, datetime: {System.DateTime.Now}");
+        }
+    }
+}
